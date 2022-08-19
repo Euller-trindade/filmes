93812e97242e274757af42c8a0d625456d5ddcec
euller trindade <eullercarregosa@gmail.com>
2022-08-19 19:54:09 -0300

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./NavBar.css";
+
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> FILMES ST
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
+export default Navbar;
